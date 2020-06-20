c1e5fc2f88de0ea7102fec8e6de2ab95cf94e2a9
Дима <mamleev-02@mail.ru>
2020-06-20 06:23:39 +0300

    first commit

---
+++ Excel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _Application excel = new _Excel.Application();
+            Workbook wb;
+            Worksheet ws;
+            int i = 1, j = 1;
+            wb = excel.Workbooks.Open(@"C:\Users\guest\Desktop\Мамлеев.xlsx");
+            ws = wb.Worksheets[1];
+            ws.Cells[i, j].Value2 = "Мамлеев Дмитрий";
+            wb.SaveAs(@"C:\Users\guest\Desktop\Мамлеев.xlsx");
+            wb.Close();
+
+
+        }
+    }
+}
